9a4b51171298ba004cb9ff7e5d839b6c9bb154bc
Otaviano07 <otaviano.silva07@hotmail.com>
2023-06-14 11:55:12 -0300

    Atualizado javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 //Remove os acentos da string
 function removerAcentos(texto) {
-    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
 //Formata a string, sem nenhum espaço substituindo por "-",somente letras minuscula
 function gerarSlug(texto) {
-    var slug = removerAcentos(texto.toLowerCase()).replace(/[^a-z0-9]+/g, "-");
-    return slug;
+  var slug = removerAcentos(texto.toLowerCase()).replace(/[^a-z0-9]+/g, "-");
+  return slug;
 }
 
 //Mascara para telefone
@@ -21,8 +21,12 @@ function mascaraTelefone(input) {
 
 //Gera o link para enviar estudo
 function gerarLink() {
-  var nomeEstudoBiblico = gerarSlug(document.getElementById("nomeEstudoBiblico").value);
-  var temaEstudoBiblico = gerarSlug(document.getElementById("temaEstudoBiblico").value);
+  var nomeEstudoBiblico = gerarSlug(
+    document.getElementById("nomeEstudoBiblico").value
+  );
+  var temaEstudoBiblico = gerarSlug(
+    document.getElementById("temaEstudoBiblico").value
+  );
   var nameProfessor = document.getElementById("nameProfessor").value;
   var nameEstudante = document.getElementById("nameEstudante").value;
   var contatoProfessor = document.getElementById("contatoProfessor").value;
@@ -39,7 +43,20 @@ function gerarLink() {
   ) {
     var linkBase = "https://otaviano07.github.io/aprendendomais/";
 
-    var linkGerado = linkBase + nomeEstudoBiblico + '/' + temaEstudoBiblico + '/index.html' + "?nameProfessor=" + encodeURIComponent(nameProfessor) + "&contatoProfessor=" + contatoProfessor + "&nameEstudante=" + encodeURIComponent(nameEstudante) + "&contatoEstudante=" + contatoEstudante;
+    var linkGerado =
+      linkBase +
+      nomeEstudoBiblico +
+      "/" +
+      temaEstudoBiblico +
+      "/index.html" +
+      "?nameProfessor=" +
+      encodeURIComponent(nameProfessor) +
+      "&contatoProfessor=" +
+      contatoProfessor +
+      "&nameEstudante=" +
+      encodeURIComponent(nameEstudante) +
+      "&contatoEstudante=" +
+      contatoEstudante;
 
     document.getElementById("linkGerado").value = linkGerado;
     document.getElementById("url").value = linkGerado;
@@ -47,24 +64,93 @@ function gerarLink() {
     alert("Por favor, preencha todos os campos antes de gerar o link.");
   }
 }
-
-
+/*
 function copiarLink() {
-  var linkGerado = document.getElementById('linkGerado');
-  linkGerado.select();
-  linkGerado.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  alert('Link copiado para a área de transferência!');
-}
+    var nomeEstudoBiblico = gerarSlug(
+    document.getElementById("nomeEstudoBiblico").value
+  );
+  var temaEstudoBiblico = gerarSlug(
+    document.getElementById("temaEstudoBiblico").value
+  );
+  var nameProfessor = document.getElementById("nameProfessor").value;
+  var nameEstudante = document.getElementById("nameEstudante").value;
+  var contatoProfessor = document.getElementById("contatoProfessor").value;
+  var contatoEstudante = document.getElementById("contatoEstudante").value;
 
-const nomeSelect = document.getElementById('nomeEstudoBiblico');
-const numeroSelect = document.getElementById('temaEstudoBiblico');
+  // Verificar se todos os campos estão preenchidos
+  if (
+    nomeEstudoBiblico &&
+    temaEstudoBiblico &&
+    nameProfessor &&
+    nameEstudante &&
+    contatoProfessor &&
+    contatoEstudante
+  ) {
+    var linkGerado = document.getElementById("linkGerado");
+    linkGerado.select();
+    linkGerado.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Link copiado para a área de transferência!");
+  } else {
+    alert("Por favor, preencha todos os campos antes de gerar o link.");
+  }
+}*/
+
+const nomeSelect = document.getElementById("nomeEstudoBiblico");
+const numeroSelect = document.getElementById("temaEstudoBiblico");
 const numeroPorNome = {
-  'Ouvindo a Voz de Deus': ['01 - A Bíblia Sagrada', '02 - A beleza da criação', '03 - A origem do mal', '04 - O plano da salvação', '05 - Fé, arrependimento e confissão', '06 - Sinais da volta de Cristo', '07 - A volta de Cristo', '08 - O Milênio', '09 - A verdade sobre a morte', '10 - A nova terra', '11 - Salvação pela graça', '12 - O Santuário', '13 - O Juízo', '14 - As leis na Bíblia', '15 - A Lei Moral', '16 - O mandamento esquecido', '17 - Do sábado para o domingo', '18 - Princípios de Saúde', '19 - O dom de profecia', '20 - O dízimo', '21 - As ofertas', '22 - A igreja verdadeira', '23 - O Batismo', '24 - Princípios de vida Cristã', '25 - Educação Cristã', '26 - Vida no Espírito', '27 - Ministério para Todos'],
-  'Ensinos de Jesus': ['01 - O que JESUS ensinou sobre as ESCRITURAS', '02 - O que JESUS ensinou sobre a SALVAÇÃO', '03 - O que JESUS ensinou sobre a SANTIFICAÇÃO', '04 - O que JESUS ensinou sobre o ESPÍRITO SANTO', '05 - O que JESUS ensinou sobre a ORAÇÃO', '06 - O que JESUS ensinou sobre o PERDÃO', '07 - O que JESUS ensinou sobre o DINHEIRO', '08 - O que JESUS ensinou sobre SUA VOLTA', '09 - O que JESUS ensinou sobre a MORTE', '10 - O que JESUS ensinou sobre a RESSURREIÇÃO', '11 - O que JESUS ensinou sobre o JUÍZO', '12 - O que JESUS ensinou sobre a LEI', '13 - O que JESUS ensinou sobre o SÁBADO', '14 - O que JESUS ensinou sobre a IGREJA', '15 - O que JESUS ensinou sobre os PROFETAS', '16 - O que JESUS ensinou sobre o BATISMO', '17 - O que JESUS ensinou sobre o CUIDADO COM O CORPO', '18 - O que JESUS ensinou sobre ritos e TRADIÇÕES']
+  "Ouvindo a Voz de Deus": [
+    "01 - A Bíblia Sagrada",
+    "02 - A beleza da criação",
+    "03 - A origem do mal",
+    "04 - O plano da salvação",
+    "05 - Fé, arrependimento e confissão",
+    "06 - Sinais da volta de Cristo",
+    "07 - A volta de Cristo",
+    "08 - O Milênio",
+    "09 - A verdade sobre a morte",
+    "10 - A nova terra",
+    "11 - Salvação pela graça",
+    "12 - O Santuário",
+    "13 - O Juízo",
+    "14 - As leis na Bíblia",
+    "15 - A Lei Moral",
+    "16 - O mandamento esquecido",
+    "17 - Do sábado para o domingo",
+    "18 - Princípios de Saúde",
+    "19 - O dom de profecia",
+    "20 - O dízimo",
+    "21 - As ofertas",
+    "22 - A igreja verdadeira",
+    "23 - O Batismo",
+    "24 - Princípios de vida Cristã",
+    "25 - Educação Cristã",
+    "26 - Vida no Espírito",
+    "27 - Ministério para Todos",
+  ],
+  "Ensinos de Jesus": [
+    "01 - O que JESUS ensinou sobre as ESCRITURAS",
+    "02 - O que JESUS ensinou sobre a SALVAÇÃO",
+    "03 - O que JESUS ensinou sobre a SANTIFICAÇÃO",
+    "04 - O que JESUS ensinou sobre o ESPÍRITO SANTO",
+    "05 - O que JESUS ensinou sobre a ORAÇÃO",
+    "06 - O que JESUS ensinou sobre o PERDÃO",
+    "07 - O que JESUS ensinou sobre o DINHEIRO",
+    "08 - O que JESUS ensinou sobre SUA VOLTA",
+    "09 - O que JESUS ensinou sobre a MORTE",
+    "10 - O que JESUS ensinou sobre a RESSURREIÇÃO",
+    "11 - O que JESUS ensinou sobre o JUÍZO",
+    "12 - O que JESUS ensinou sobre a LEI",
+    "13 - O que JESUS ensinou sobre o SÁBADO",
+    "14 - O que JESUS ensinou sobre a IGREJA",
+    "15 - O que JESUS ensinou sobre os PROFETAS",
+    "16 - O que JESUS ensinou sobre o BATISMO",
+    "17 - O que JESUS ensinou sobre o CUIDADO COM O CORPO",
+    "18 - O que JESUS ensinou sobre ritos e TRADIÇÕES",
+  ],
 };
 
-nomeSelect.addEventListener('change', function () {
+nomeSelect.addEventListener("change", function () {
   const selectedNome = this.value;
   const numeros = numeroPorNome[selectedNome] || [];
 
@@ -73,17 +159,17 @@ nomeSelect.addEventListener('change', function () {
 
   // Adicionar as novas opções filtradas
   numeros.forEach(function (numero) {
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.value = numero;
     option.textContent = numero;
     numeroSelect.appendChild(option);
   });
 });
 
-
 //Encurtador de link
 function shortenUrl(longUrl, customSlug) {
-  var apiUrl = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(longUrl);
+  var apiUrl =
+    "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(longUrl);
   if (customSlug) {
     apiUrl += "&alias=" + encodeURIComponent(customSlug);
   }
@@ -91,17 +177,14 @@ function shortenUrl(longUrl, customSlug) {
   //Seria para colocar a url original em id="url"
   //document.getElementById("linkGerado").value = longUrl;
   //document.getElementById("url").value = apiUrl;
-  
+
   return fetch(apiUrl)
-    .then(function(response) {
+    .then(function (response) {
       return response.text();
-    
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log("Erro ao encurtar o link:", error);
     });
-
-    
 }
 
 function updateUrl(shortUrl, longUrl) {
@@ -113,56 +196,164 @@ function updateUrl(shortUrl, longUrl) {
 }
 
 function encurtarLink() {
-  var urlInput = document.getElementById("url");
-  var urlLongo = urlInput.value;
+  var nomeEstudoBiblico = gerarSlug(
+    document.getElementById("nomeEstudoBiblico").value
+  );
+  var temaEstudoBiblico = gerarSlug(
+    document.getElementById("temaEstudoBiblico").value
+  );
+  var nameProfessor = document.getElementById("nameProfessor").value;
+  var nameEstudante = document.getElementById("nameEstudante").value;
+  var contatoProfessor = document.getElementById("contatoProfessor").value;
+  var contatoEstudante = document.getElementById("contatoEstudante").value;
 
-  shortenUrl(urlLongo)
-    .then(function(shortUrl) {
+  // Verificar se todos os campos estão preenchidos
+  if (
+    nomeEstudoBiblico &&
+    temaEstudoBiblico &&
+    nameProfessor &&
+    nameEstudante &&
+    contatoProfessor &&
+    contatoEstudante
+  ) {
+    var urlInput = document.getElementById("url");
+    var urlLongo = urlInput.value;
+
+    shortenUrl(urlLongo).then(function (shortUrl) {
       updateUrl(shortUrl, urlLongo);
     });
     document.getElementById("url").value = "";
+  } else {
+    alert("Por favor, preencha todos os campos antes de gerar o link.");
+  }
 }
 
 //Copia o link para outra area
 function copiarLink() {
-  var linkGeradoInput = document.getElementById("linkGerado");
-  linkGeradoInput.select();
+  var nomeEstudoBiblico = gerarSlug(
+    document.getElementById("nomeEstudoBiblico").value
+  );
+  var temaEstudoBiblico = gerarSlug(
+    document.getElementById("temaEstudoBiblico").value
+  );
+  var nameProfessor = document.getElementById("nameProfessor").value;
+  var nameEstudante = document.getElementById("nameEstudante").value;
+  var contatoProfessor = document.getElementById("contatoProfessor").value;
+  var contatoEstudante = document.getElementById("contatoEstudante").value;
 
-  if (navigator.clipboard) {
-    navigator.clipboard.writeText(linkGeradoInput.value)
-      .then(function () {
-        console.log("Link copiado com sucesso.");
-      })
-      .catch(function (error) {
-        console.log("Erro ao copiar o link:", error);
-      });
+  // Verificar se todos os campos estão preenchidos
+  if (
+    nomeEstudoBiblico &&
+    temaEstudoBiblico &&
+    nameProfessor &&
+    nameEstudante &&
+    contatoProfessor &&
+    contatoEstudante
+  ) {
+    var linkGeradoInput = document.getElementById("linkGerado");
+    linkGeradoInput.select();
+
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(linkGeradoInput.value)
+        .then(function () {
+          console.log("Link copiado com sucesso.");
+        })
+        .catch(function (error) {
+          console.log("Erro ao copiar o link:", error);
+        });
+    } else {
+      // Fallback para navegadores que não suportam a API Clipboard
+      var tempInput = document.createElement("input");
+      tempInput.value = linkGeradoInput.value;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+    }
   } else {
-    // Fallback para navegadores que não suportam a API Clipboard
-    var tempInput = document.createElement("input");
-    tempInput.value = linkGeradoInput.value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
+    alert("Por favor, preencha todos os campos antes de gerar o link.");
   }
 }
 
 //Envia a mensagem para o estudante pelo whatsapp
 function enviarMensagemWhatsapp() {
-  var numeroEstudante = document.getElementById("contatoEstudante").value;
-  var linkGerado = document.getElementById("linkGerado").value;
-  var mensagem = "Olá! Aqui está o link para o estudo: " + linkGerado;
-  
-  if (isDispositivoMobile()) {
-    var url = "https://api.whatsapp.com/send?phone=55" + numeroEstudante + "&text=" + encodeURIComponent(mensagem);
+  var nomeEstudoBiblico = gerarSlug(
+    document.getElementById("nomeEstudoBiblico").value
+  );
+  var temaEstudoBiblico = gerarSlug(
+    document.getElementById("temaEstudoBiblico").value
+  );
+  var nameProfessor = document.getElementById("nameProfessor").value;
+  var nameEstudante = document.getElementById("nameEstudante").value;
+  var contatoProfessor = document.getElementById("contatoProfessor").value;
+  var contatoEstudante = document.getElementById("contatoEstudante").value;
+
+  // Verificar se todos os campos estão preenchidos
+  if (
+    nomeEstudoBiblico &&
+    temaEstudoBiblico &&
+    nameProfessor &&
+    nameEstudante &&
+    contatoProfessor &&
+    contatoEstudante
+  ) {
+    var numeroEstudante = document.getElementById("contatoEstudante").value;
+    var linkGerado = document.getElementById("linkGerado").value;
+    var mensagem = "Olá! Aqui está o link para o estudo: " + linkGerado;
+
+    if (isDispositivoMobile()) {
+      var url =
+        "https://api.whatsapp.com/send?phone=55" +
+        numeroEstudante +
+        "&text=" +
+        encodeURIComponent(mensagem);
+    } else {
+      var url =
+        "https://web.whatsapp.com/send/?phone=55" +
+        numeroEstudante +
+        "&text=" +
+        encodeURIComponent(mensagem);
+    }
+
+    window.open(url, "_blank");
   } else {
-    var url = "https://web.whatsapp.com/send/?phone=55" + numeroEstudante + "&text=" + encodeURIComponent(mensagem);
+    alert("Por favor, preencha todos os campos antes de gerar o link.");
   }
-  
-  window.open(url, "_blank");
 }
 
 //Verifica se é dispositivo mobile
 function isDispositivoMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+}
+
+//Abrir Link gerado
+function abrirLink() {
+  var nomeEstudoBiblico = gerarSlug(
+    document.getElementById("nomeEstudoBiblico").value
+  );
+  var temaEstudoBiblico = gerarSlug(
+    document.getElementById("temaEstudoBiblico").value
+  );
+  var nameProfessor = document.getElementById("nameProfessor").value;
+  var nameEstudante = document.getElementById("nameEstudante").value;
+  var contatoProfessor = document.getElementById("contatoProfessor").value;
+  var contatoEstudante = document.getElementById("contatoEstudante").value;
+
+  // Verificar se todos os campos estão preenchidos
+  if (
+    nomeEstudoBiblico &&
+    temaEstudoBiblico &&
+    nameProfessor &&
+    nameEstudante &&
+    contatoProfessor &&
+    contatoEstudante
+  ) {
+  var linkGerado = document.getElementById("linkGerado").value;
+  window.open(linkGerado, "_blank");
+} else {
+  alert("Por favor, preencha todos os campos antes de gerar o link.");
+}
 }
