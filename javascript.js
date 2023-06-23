9b0d1c4adb350dbd22b5b8f0b8000efaccc959f8
Otaviano07 <otaviano.silva07@hotmail.com>
2023-06-23 08:56:54 -0300

    Atualizado mensagens antes de gerar, encurtar, copiar, abrir e mensagem

--- javascript.js
+++ javascript.js
@@ -352,7 +352,7 @@ function encurtarLink() {
     }
   } else {
     alert(
-      "Por favor, preencha todos os campos antes de gerar o link. Para depois clicar em Gerar Link."
+      "Por favor, preencha todos os campos antes de encurtar o link. Para depois clicar em Gerar Link."
     );
   }
 }
@@ -424,7 +424,7 @@ function copiarLink() {
     }
   } else {
     alert(
-      "Por favor, preencha todos os campos antes de gerar o link. Para depois clicar em Gerar Link."
+      "Por favor, preencha todos os campos antes de copiar o link. Para depois clicar em Gerar Link."
     );
   }
 }
@@ -500,7 +500,7 @@ function enviarMensagemWhatsapp() {
     }
   } else {
     alert(
-      "Por favor, preencha todos os campos antes de gerar o link. Para depois clicar em Gerar Link"
+      "Por favor, preencha todos os campos antes de enviar mensagem para o Whatsapp. Para depois clicar em Gerar Link"
     );
   }
 }
@@ -544,7 +544,7 @@ function abrirLink() {
     }
   } else {
     alert(
-      "Por favor, preencha todos os campos antes de gerar o link. Para depois clicar em Gerar Link."
+      "Por favor, preencha todos os campos antes de abrir o link. Para depois clicar em Gerar Link."
     );
   }
 }
